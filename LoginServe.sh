98201184e0d1b16224fdd8cf5a9999c995d9c92d
savent <savent_gate@outlook.com>
2016-11-21 10:10:32 +0800

    add a simple sh file

---
+++ LoginServe.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+OUTPUT_PIN = 4
+GPIO_OUT   = gpio4
+INPUT_PIN  = 5
+GPIO_IN    = gpio5
+
+start () {
+    echo OUTPUT_PIN > /sys/class/gpio/export
+    echo INPUT_PIN  > /sys/class/gpio/export
+    echo out        > /sys/class/gpio/GPIO_OUT/direction
+    echo 1          > /sys/class/gpio/GPIO_OUT/value
+    return 0
+}
+
+stop () {
+    echo OUTPUT_PIN > /sys/class/gpio/unexport
+    echo INPUT_PIN  > /sys/class/gpio/unexport
+    return 0
+}
+
+out1 () {
+    echo 1          > /sys/class/gpio/GPIO_OUT/value
+    return 0
+}
+
+out0 () {
+    echo 0          > /sys/class/gpio/GPIO_OUT/value
+}
+
+read () {
+    echo /sys/class/gpio/GPIO_IN/value
+    return /sys/class/gpio/GPIO_IN/value
+}
+
+open () {
+    out0
+    sleep 1
+    out1
+    return 0
+}
